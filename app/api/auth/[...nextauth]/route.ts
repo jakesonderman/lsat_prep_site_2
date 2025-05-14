68d3ea545ee7de70ffa156c4e3241ee770997c59
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2025-05-14 15:59:58 -0400

    fixing nextauth route file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -65,6 +65,8 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
+// Create NextAuth handler
 const handler = NextAuth(authOptions);
 
+// Export the handler as GET and POST
 export { handler as GET, handler as POST }; 
