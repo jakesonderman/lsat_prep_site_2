7d964b5001ba3872326a6d2ba134db2b97f55a4d
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2025-05-14 14:40:44 -0400

    Fixcies pt.4

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../app/components/Layout';
+import Dashboard from '../app/components/Dashboard';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+} 
