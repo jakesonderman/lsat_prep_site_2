3c00eed5b781f4a08b22f72acffa396026f3e4d1
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2025-05-14 16:08:12 -0400

    fixing more auth request errors

--- app/api/user-data/route.ts
+++ app/api/user-data/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import clientPromise from '../../lib/mongodb';
 import { UserData } from '../../lib/models';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '../../lib/auth';
 
 // Get user data
 export async function GET(request: Request) {
